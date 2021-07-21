ffd6be58f5c0b4a2fe9c23679105e5fa69e66803
Karim Dilling <ktk.dilling@freenet.de>
2021-07-21 21:04:14 +0200

    Ball now bounces upon hitting window borders

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from pygame.constants import K_DOWN, K_UP
+import random
 
 pygame.init()
 
@@ -18,6 +19,10 @@ player1 = pygame.Rect(0, WIN_HEIGHT / 2 - 70, 20, 140)
 player2 = pygame.Rect(WIN_WIDTH - 20, WIN_HEIGHT / 2 - 70, 20, 140)
 ball = pygame.Rect(WIN_WIDTH / 2 - 20, WIN_HEIGHT / 2 - 20, 40, 40)
 
+# Define speed of the ball
+ball_speed_x = 5
+ball_speed_y = 5
+
 def draw_shapes():
     pygame.draw.rect(WINDOW, "white", player1)
     pygame.draw.rect(WINDOW, "white", player2)
@@ -30,13 +35,22 @@ def setup_keylistener():
     if keys_pressed[K_DOWN]:
         player1.y += 8
 
-# Game loop
-while True:
+def handle_quitting():
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
+
+# Game loop
+while True:
+    handle_quitting()
     setup_keylistener()
+    ball.x += ball_speed_x
+    ball.y += ball_speed_y
+    if ball.top <= 0 or ball.bottom >= WIN_HEIGHT:
+        ball_speed_y *= -1
+    if ball.left <= 0 or ball.right >= WIN_WIDTH:
+        ball_speed_x *= -1
     WINDOW.fill("black")
     draw_shapes()
     pygame.display.update()
